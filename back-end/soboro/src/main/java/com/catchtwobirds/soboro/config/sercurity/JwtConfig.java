c542ab7b56cac97f29a0a894acde336e8cb9eaef
White-Asher <dkxmpower999@naver.com>
2023-03-17 07:38:48 +0900

    comment: config, util 주석추가

--- back-end/soboro/src/main/java/com/catchtwobirds/soboro/config/sercurity/JwtConfig.java
+++ back-end/soboro/src/main/java/com/catchtwobirds/soboro/config/sercurity/JwtConfig.java
@@ -1,5 +1,6 @@
 package com.catchtwobirds.soboro.config.sercurity;
 
+import com.catchtwobirds.soboro.auth.token.AuthTokenProvider;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- back-end/soboro/src/main/java/com/catchtwobirds/soboro/utils/RedisUtil.java
+++ back-end/soboro/src/main/java/com/catchtwobirds/soboro/utils/RedisUtil.java
@@ -8,6 +8,15 @@ import org.springframework.stereotype.Component;
 
 import java.time.Duration;
 
+/**
+ * 이 코드는 Redis 데이터베이스를 사용하여 데이터를 읽고 쓰는 RedisUtil 클래스를 정의하는 Java 코드입니다. <br>
+ * RedisUtil 클래스는 StringRedisTemplate과 RedisTemplate<String, Object> 필드를 가지고 있습니다. <br>
+ * getData 메서드는 StringRedisTemplate의 opsForValue() 메서드를 사용하여 Redis에서 특정 키에 대한 값을 읽어옵니다. <br>
+ * setData 메서드는 StringRedisTemplate의 opsForValue() 메서드를 사용하여 Redis에 특정 키에 대한 값을 설정합니다. <br>
+ * setDataExpire 메서드는 StringRedisTemplate의 opsForValue() 메서드를 사용하여 Redis에 특정 키에 대한 값을 설정하고, 지정된 시간 후에 만료되도록 설정합니다. <br>
+ * delData 메서드는 RedisTemplate의 delete() 메서드를 사용하여 Redis에서 특정 키와 연결된 데이터를 삭제합니다. <br>
+ */
+
 @Component
 @RequiredArgsConstructor
 public class RedisUtil {
