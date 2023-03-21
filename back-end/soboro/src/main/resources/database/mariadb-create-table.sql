aeb83d6aa840aa4ae7612a446983a186ba4ed9c7
황도영 <ghkdehdud96@naver.com>
2023-03-21 17:51:45 +0900

    fix : DB 테이블 작성코드 수정

--- back-end/soboro/src/main/resources/database/mariadb-create-table.sql
+++ back-end/soboro/src/main/resources/database/mariadb-create-table.sql
@@ -1,26 +1,28 @@
 use soborodb;
-create table user(
-                     `user_no` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                     `user_id` varchar(15),
-                     `user_password` varchar(255),
-                     `user_name` varchar(50),
-                     `user_email` varchar(100),
-                     `user_phone` varchar(50),
-                     `user_birthdate` char(8),
-                     `user_gender` char(1),
-                     `user_terms` boolean,
-                     `user_provide_type` varchar(20),
-                     `user_role_type` varchar(20),
-                     `user_created_time` timestamp,
-                     `user_active` boolean
+CREATE TABLE `user` (
+                    `user_no` int(11) NOT NULL AUTO_INCREMENT,
+                    `user_id` varchar(15) DEFAULT NULL,
+                    `user_password` varchar(255) DEFAULT NULL,
+                    `user_name` varchar(50) DEFAULT NULL,
+                    `user_email` varchar(100) DEFAULT NULL,
+                    `user_phone` varchar(50) DEFAULT NULL,
+                    `user_birthdate` char(8) DEFAULT NULL,
+                    `user_gender` char(1) DEFAULT NULL,
+                    `user_terms` tinyint(1) DEFAULT NULL,
+                    `user_provide_type` varchar(20) DEFAULT NULL,
+                    `user_role_type` varchar(20) DEFAULT NULL,
+                    `user_created_time` timestamp NULL DEFAULT NULL,
+                    `user_active` tinyint(1) DEFAULT NULL,
+                    PRIMARY KEY (`user_no`)
 );
-
-create table consulting(
-                   `consulting_no` int NOT NULL AUTO_INCREMENT primary key,
-                   `consulting_id` int,
-                   `consulting_visit_date` timestamp,
-                   `consulting_visit_place` varchar(100),
-                   `consulting_visit_class` varchar(100),
-                   `video_location` varchar(255),
-                   CONSTRAINT user_no foreign key (consulting_id) REFERENCES user (user_no) on delete cascade on update cascade
-);
+CREATE TABLE `consulting` (
+                      `consulting_no` int(11) NOT NULL AUTO_INCREMENT,
+                      `consulting_user_no` int(11) DEFAULT NULL,
+                      `consulting_visit_date` timestamp NULL DEFAULT NULL,
+                      `consulting_visit_place` varchar(100) DEFAULT NULL,
+                      `consulting_visit_class` varchar(100) DEFAULT NULL,
+                      `video_location` varchar(255) DEFAULT NULL,
+                      PRIMARY KEY (`consulting_no`),
+                      KEY `fk_user_userno` (`consulting_user_no`),
+                      CONSTRAINT `fk_user_userno` FOREIGN KEY (`consulting_user_no`) REFERENCES `user` (`user_no`) ON DELETE CASCADE ON UPDATE CASCADE
+)
