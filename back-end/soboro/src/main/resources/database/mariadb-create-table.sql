f1a4d7a521898096f2fc774462aa2c2a73b9ff7f
White-Asher <dkxmpower999@naver.com>
2023-03-28 10:42:00 +0900

    fix : create table.sql

--- back-end/soboro/src/main/resources/database/mariadb-create-table.sql
+++ back-end/soboro/src/main/resources/database/mariadb-create-table.sql
@@ -90,6 +90,7 @@ CREATE TABLE `consulting` (
 >>>>>>> 13a8ce5 (Merge branch 'jaeyoon' into 'BE')
 );
 
+<<<<<<< HEAD
 CREATE TABLE `emailauth` (
                              `emailauth_no` int NOT NULL AUTO_INCREMENT,
                              `emailauth_email` varchar(50) DEFAULT NULL,
@@ -101,3 +102,28 @@ CREATE TABLE `emailauth` (
 =======
 ) ;
 >>>>>>> 13a8ce5 (Merge branch 'jaeyoon' into 'BE')
+=======
+LOCK TABLES `consulting` WRITE;
+/*!40000 ALTER TABLE `consulting` DISABLE KEYS */;
+INSERT INTO `consulting` VALUES
+                             (1,1,'2023-03-20 10:53:35','봉명동 은행1','은행','www.video.com'),
+                             (2,1,'2023-03-20 10:53:35','봉명동 우체국1','은행','www.video.com'),
+                             (3,2,'2023-03-22 11:04:34','봉명동 동사무소','동사무소','www.video.com'),
+                             (4,2,'2023-03-20 10:53:35','봉명동 우체국','은행','www.video.com'),
+                             (5,1,'2023-03-21 10:53:35','봉명동 도서관1','도서관','www.video.com'),
+                             (6,1,'2023-03-21 12:53:35','봉명동 동사무소1','복지센터','www.video.com'),
+                             (7,1,'2023-03-20 10:53:35','봉명동 은행2','은행','www.video.com'),
+                             (8,1,'2023-03-20 10:53:35','봉명동 우체국2','은행','www.video.com'),
+                             (9,1,'2023-03-22 11:04:34','봉명동 동사무소2','동사무소','www.video.com'),
+                             (10,1,'2023-03-20 10:53:35','봉명동 우체국3','은행','www.video.com'),
+                             (11,1,'2023-03-21 10:53:35','봉명동 도서관3','도서관','www.video.com'),
+                             (12,1,'2023-03-21 12:53:35','봉명동 동사무소3','복지센터','www.video.com'),
+                             (13,1,'2023-03-20 10:53:35','봉명동 은행4','은행','www.video.com'),
+                             (14,1,'2023-03-20 10:53:35','봉명동 우체국4','은행','www.video.com'),
+                             (15,1,'2023-03-22 11:04:34','봉명동 동사무소4','동사무소','www.video.com'),
+                             (16,1,'2023-03-20 10:53:35','봉명동 우체국5','은행','www.video.com'),
+                             (17,1,'2023-03-21 10:53:35','봉명동 도서관5','도서관','www.video.com'),
+                             (18,1,'2023-03-21 12:53:35','봉명동 동사무소5','복지센터','www.video.com');
+/*!40000 ALTER TABLE `consulting` ENABLE KEYS */;
+UNLOCK TABLES;
+>>>>>>> f6d1c1d (fix : create table.sql)
