a77373e5bb785cb11a368ef0544a5c3317898ab1
White-Asher <dkxmpower999@naver.com>
2023-03-30 13:38:09 +0900

    fix : 충돌 커밋 삭제 후 누락파일 추가

--- back-end/soboro/src/main/java/com/catchtwobirds/soboro/content/dto/ContentResponseDto.java
+++ back-end/soboro/src/main/java/com/catchtwobirds/soboro/content/dto/ContentResponseDto.java
@@ -13,33 +13,9 @@ import org.bson.types.ObjectId;
 @ToString
 @Schema(description = "상담 텍스트 응답 DTO")
 public class ContentResponseDto {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-    @Schema(description = "상담 텍스트 식별번호")
-    private ObjectId contentNo;
-=======
-//    @Schema(description = "상담 텍스트 식별번호")
+    //    @Schema(description = "상담 텍스트 식별번호")
 //    private ObjectId contentNo;
->>>>>>> ba39b7f (chore : 상담내역 DTO 스키마 수정)
-=======
-//    @Schema(description = "상담 텍스트 식별번호")
-//    private ObjectId contentNo;
->>>>>>> a6a0cde (Merge branch 'BE' of https://lab.ssafy.com/s08-ai-speech-sub2/S08P22B202 into jaeyoon)
     @Schema(description = "상담 식별번호")
->>>>>>> bf67778 (fix : Dto schema 어노테이션 정의)
-=======
-    @Schema(description = "상담 텍스트 식별번호")
-    private ObjectId contentNo;
-=======
-//    @Schema(description = "상담 텍스트 식별번호")
-//    private ObjectId contentNo;
->>>>>>> 678bb58 (Merge branch 'doyeong' into 'BE')
-    @Schema(description = "상담 식별번호")
->>>>>>> c72068f (Merge branch 'jaeyoon' into 'BE')
     private Integer consultingNo;
     @Schema(description = "상담 텍스트 내용")
     private String contentText;
--- back-end/soboro/src/main/resources/database/mariadb-create-table.sql
+++ back-end/soboro/src/main/resources/database/mariadb-create-table.sql
@@ -1,44 +1,5 @@
 use soborodb;
 CREATE TABLE `user` (
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> a04fb1d (Merge branch 'doyeong' into 'BE')
-=======
->>>>>>> 37bf35b (Merge branch 'BE' into 'jaeyoon')
-                    `user_no` int(11) NOT NULL AUTO_INCREMENT,
-                    `user_id` varchar(15) DEFAULT NULL,
-                    `user_password` varchar(255) DEFAULT NULL,
-                    `user_name` varchar(50) DEFAULT NULL,
-                    `user_email` varchar(100) DEFAULT NULL,
-                    `user_phone` varchar(50) DEFAULT NULL,
-                    `user_birthdate` char(8) DEFAULT NULL,
-                    `user_gender` char(1) DEFAULT NULL,
-                    `user_terms` tinyint(1) DEFAULT NULL,
-                    `user_provide_type` varchar(20) DEFAULT NULL,
-                    `user_role_type` varchar(20) DEFAULT NULL,
-                    `user_created_time` timestamp NULL DEFAULT NULL,
-                    `user_active` tinyint(1) DEFAULT NULL,
-                    PRIMARY KEY (`user_no`)
-);
-<<<<<<< HEAD
-CREATE TABLE `consulting` (
-                      `consulting_no` int(11) NOT NULL AUTO_INCREMENT,
-                      `consulting_user_no` int(11) DEFAULT NULL,
-                      `consulting_visit_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-                      `consulting_visit_place` varchar(100) DEFAULT NULL,
-                      `consulting_visit_class` varchar(100) DEFAULT NULL,
-                      `video_location` varchar(255) DEFAULT NULL,
-                      PRIMARY KEY (`consulting_no`),
-                      KEY `fk_user_userno` (`consulting_user_no`),
-                      CONSTRAINT `fk_user_userno` FOREIGN KEY (`consulting_user_no`) REFERENCES `user` (`user_no`) ON DELETE CASCADE ON UPDATE CASCADE
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-)
-=======
                         `user_no` int(11) NOT NULL AUTO_INCREMENT,
                         `user_id` varchar(15) DEFAULT NULL,
                         `user_password` varchar(255) DEFAULT NULL,
@@ -53,64 +14,19 @@ CREATE TABLE `consulting` (
                         `user_created_time` timestamp NULL DEFAULT NULL,
                         `user_active` tinyint(1) DEFAULT NULL,
                         PRIMARY KEY (`user_no`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-insert into user
-values (1, 'test', '$2a$10$0AycQUE0MPdxY7zTylZmJebhQ1dqNksMhA4lrmg52TiefKPbV.Zju', 'testName', 'testEmail@Eamil.com', '01012345678', '20220101', 'N', 1, 'LOCAL', 'USER', '2023-03-20 10:53:35', 1);
-
+);
 CREATE TABLE `consulting` (
                               `consulting_no` int(11) NOT NULL AUTO_INCREMENT,
                               `consulting_user_no` int(11) DEFAULT NULL,
-                              `consulting_visit_date` timestamp NULL DEFAULT NULL,
+                              `consulting_visit_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
                               `consulting_visit_place` varchar(100) DEFAULT NULL,
                               `consulting_visit_class` varchar(100) DEFAULT NULL,
                               `video_location` varchar(255) DEFAULT NULL,
                               PRIMARY KEY (`consulting_no`),
                               KEY `fk_user_userno` (`consulting_user_no`),
                               CONSTRAINT `fk_user_userno` FOREIGN KEY (`consulting_user_no`) REFERENCES `user` (`user_no`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
->>>>>>> 0a72c92 (docs: sql table 수정)
-=======
-)
->>>>>>> a04fb1d (Merge branch 'doyeong' into 'BE')
-=======
-CREATE TABLE `consulting` (
-                      `consulting_no` int(11) NOT NULL AUTO_INCREMENT,
-                      `consulting_user_no` int(11) DEFAULT NULL,
-                      `consulting_visit_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-                      `consulting_visit_place` varchar(100) DEFAULT NULL,
-                      `consulting_visit_class` varchar(100) DEFAULT NULL,
-                      `video_location` varchar(255) DEFAULT NULL,
-                      PRIMARY KEY (`consulting_no`),
-                      KEY `fk_user_userno` (`consulting_user_no`),
-                      CONSTRAINT `fk_user_userno` FOREIGN KEY (`consulting_user_no`) REFERENCES `user` (`user_no`) ON DELETE CASCADE ON UPDATE CASCADE
-)
->>>>>>> 37bf35b (Merge branch 'BE' into 'jaeyoon')
-=======
-=======
->>>>>>> 13a8ce5 (Merge branch 'jaeyoon' into 'BE')
 );
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-CREATE TABLE `emailauth` (
-                             `emailauth_no` int NOT NULL AUTO_INCREMENT,
-                             `emailauth_email` varchar(50) DEFAULT NULL,
-                             `emailauth_code` varchar(20) DEFAULT NULL,
-                             PRIMARY KEY (`emailauth_no`)
-<<<<<<< HEAD
-) ;
->>>>>>> b937cc5 (fix : 이메일 코드 발송 구현)
-=======
-) ;
->>>>>>> 13a8ce5 (Merge branch 'jaeyoon' into 'BE')
-=======
-=======
->>>>>>> b6a07b8 (Merge branch 'jaeyoon' into 'BE')
-=======
-);
-
->>>>>>> d1b55a8 (Merge branch 'BE' into 'doyeong')
 LOCK TABLES `consulting` WRITE;
 /*!40000 ALTER TABLE `consulting` DISABLE KEYS */;
 INSERT INTO `consulting` VALUES
@@ -133,13 +49,4 @@ INSERT INTO `consulting` VALUES
                              (17,1,'2023-03-21 10:53:35','봉명동 도서관5','도서관','www.video.com'),
                              (18,1,'2023-03-21 12:53:35','봉명동 동사무소5','복지센터','www.video.com');
 /*!40000 ALTER TABLE `consulting` ENABLE KEYS */;
-<<<<<<< HEAD
-<<<<<<< HEAD
 UNLOCK TABLES;
->>>>>>> f6d1c1d (fix : create table.sql)
-=======
-UNLOCK TABLES;
->>>>>>> b6a07b8 (Merge branch 'jaeyoon' into 'BE')
-=======
-UNLOCK TABLES;
->>>>>>> d1b55a8 (Merge branch 'BE' into 'doyeong')
